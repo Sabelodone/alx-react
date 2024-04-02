717e9b97bb9830b54dbcf6d89c4f43501167bfc3
Sabelodone <sabelozondo825@gmail.com>
2024-04-02 19:19:03 +0200

    commit

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,30 +1,34 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
-            },
-          },
-        ],
-      },
-    ],
-  },
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+			}
+		]
+	}
 };
