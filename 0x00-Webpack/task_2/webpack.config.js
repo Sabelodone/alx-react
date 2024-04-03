c4a5abb42e275ab5f013e6215dfb255f925fb213
Sabelodone <sabelozondo825@gmail.com>
2024-04-03 12:37:06 +0200

    webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,36 +1,34 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './task_2/js/dashboard_main.js',
-    output: {
-        path: path.resolve(__dirname, 'task_2/public'),
-        filename: 'bundle.js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource'
-            }
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './task_2/public/index.html',
-            filename: 'index.html'
-        })
-    ],
-    performance: {
-        hints: false
-    }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: false
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
