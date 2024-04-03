b14a9873d5189f02836c39392e268f0f97312f6d
Sabelodone <sabelozondo825@gmail.com>
2024-04-03 12:45:51 +0200

    main.js

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,18 +2,21 @@ import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').append(`<p>Holberton Dashboard</p>`);
-$('body').append(`<p>Dashboard data for the students</p>`);
-$('body').append(`<button>Click here to get started</button>`);
-$('body').append(`<p id="count"></p>`);
-$('body').append(`<p>Copyright - Holberton School</p>`);
+// Encapsulate the code inside an IIFE
+(function () {
+    $(document).ready(function () {
+        $('body').append(`<p>Holberton Dashboard</p>`);
+        $('body').append(`<p>Dashboard data for the students</p>`);
+        $('body').append(`<button>Click here to get started</button>`);
+        $('body').append(`<p id="count"></p>`);
+        $('body').append(`<p>Copyright - Holberton School</p>`);
 
-$('button').on('click', _.debounce(updateCounter, 500));
+        $('button').on('click', _.debounce(updateCounter, 500));
 
-let count = 0;
-function updateCounter() {
-  count++;
-  $('#count').text(`${count} clicks on the button`);
-}
-
-export { updateCounter };
+        let count = 0;
+        function updateCounter() {
+            count++;
+            $('#count').text(`${count} clicks on the button`);
+        }
+    });
+})();
